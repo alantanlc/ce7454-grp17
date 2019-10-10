89a40a0c074bc96985e6c8368b54851d5db1e7fb
Alan Tan <alanwuha91@gmail.com>
2019-10-10 17:55:06 +0800

    frontal vs lateral, count by classes

--- explore.py
+++ explore.py
@@ -3,28 +3,40 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 import seaborn as sns
-import argparse
-
 'exec(%matplotlib inline)'
 
 train = pd.read_csv('data/CheXpert-v1.0-small/train.csv')
 test = pd.read_csv('data/CheXpert-v1.0-small/valid.csv')
+labels = train.columns.values[-14:]
+print(labels)
 
-print(test.head())
-
-# Distribution of the target variable
-detected_counts = train.Fracture.value_counts()
+# Distribution of frontal and lateral images
+frontal_lateral_counts = train[train.columns.values[3]].value_counts()
 plt.figure(figsize = (12,6))
-sns.barplot(detected_counts.index, detected_counts.values, alpha = 0.9)
+sns.barplot(frontal_lateral_counts.index, frontal_lateral_counts.values, alpha = 0.9)
 plt.xticks(rotation = 'vertical')
 plt.xlabel('Fracture labels', fontsize = 12)
 plt.ylabel('Counts', fontsize = 12)
 plt.show()
 
-# Distribution of the variable 'age'
-ax = plt.figure(figsize=(30, 8))
-sns.countplot(train.Age)
-axis_font = {'fontname': 'Arial', 'size':'24'}
-plt.xlabel('age', **axis_font)
-plt.ylabel('Count', **axis_font)
+# Distribution of target variable
+label_counts = []
+plt.figure(figsize = (12,6))
+for label in labels:
+    count = train[label].value_counts().values[0]
+    label_counts.append(count)
+sns.barplot(labels, label_counts, alpha = 0.9)
+plt.xticks(rotation = 'vertical')
+plt.xlabel('Classes', fontsize=10)
+plt.ylabel('Counts', fontsize=10)
 plt.show()
+
+# Distribution of the variable 'age'
+# ax = plt.figure(figsize=(30, 8))
+# sns.countplot(train.Age)
+# axis_font = {'fontname': 'Arial', 'size':'24'}
+# plt.xlabel('age', **axis_font)
+# plt.ylabel('Count', **axis_font)
+# plt.show()
+
+print('End of program')
