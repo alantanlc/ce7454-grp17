84c48f4fedeb292d984aac9c5a1134886b66099c
Alan Tan <alanwuha91@gmail.com>
2019-10-31 08:40:36 +0800

    updated explore to use CheXpert class

--- explore.py
+++ explore.py
@@ -1,17 +1,12 @@
-import pandas as pd
 import matplotlib.pyplot as plt
-import os
-import numpy as np
 import seaborn as sns
-'exec(%matplotlib inline)'
+from dataset import CheXpertDataset
 
-train = pd.read_csv('data/CheXpert-v1.0-small/train.csv')
-test = pd.read_csv('data/CheXpert-v1.0-small/valid.csv')
-labels = train.columns.values[-14:]
-print(labels)
+train = CheXpertDataset()
+labels = train.labels_cols
 
 # Distribution of frontal and lateral images
-frontal_lateral_counts = train[train.columns.values[3]].value_counts()
+frontal_lateral_counts = train.csv.iloc[:, 3].value_counts()
 plt.figure(figsize = (12,6))
 sns.barplot(frontal_lateral_counts.index, frontal_lateral_counts.values, alpha = 0.9)
 plt.xticks(rotation = 'vertical')
@@ -22,7 +17,7 @@ plt.ylabel('Counts', fontsize = 12)
 label_counts = []
 plt.figure(figsize = (12,6))
 for label in labels:
-    count = train[label].value_counts().values[0]
+    count = train.csv.loc[:, label].value_counts().values[0]
     label_counts.append(count)
 sns.barplot(labels, label_counts, alpha = 0.9)
 plt.xticks(rotation = 'vertical')
@@ -31,7 +26,7 @@ plt.ylabel('Counts', fontsize=10)
 
 # Distribution of the variable 'age'
 ax = plt.figure(figsize=(30, 8))
-sns.countplot(train.Age)
+sns.countplot(train.csv.Age)
 axis_font = {'fontname': 'Arial', 'size':'24'}
 plt.xlabel('age', **axis_font)
 plt.ylabel('Count', **axis_font)
