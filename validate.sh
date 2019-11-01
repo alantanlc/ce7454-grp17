b5be087b4a337600aa1ccf5a3e412e4f47688a73
TangZJ <tang.zhejun@gmail.com>
2019-11-01 10:28:00 +0800

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #work_path=$(dirname $0)
-srun --mpi=pmi2 -p Sensetime -n1 --gres=gpu:8 --ntasks-per-node=8 --job-name=resnet18_20e_class5 \
-python -u validate.py --bs 256 --model SN --num_classes 5 --checkpoint /mnt/lustre/tangzhejun/CE/Datascience/experiments/kdSN20e_resnet152_zm/checkpoint.pth.tar
+srun --mpi=pmi2 -p $1 -n1 --gres=gpu:8 --ntasks-per-node=8 --job-name=resnet18_20e_class5 \
+python -u validate.py --bs 256 --model SN --num_classes 5 --checkpoint kdSN20e_resnet152_zm/checkpoint.pth.tar
 
 #--config $work_path/config.yaml \
  #--load-path=$work_path/ckpt.pth.tar \
